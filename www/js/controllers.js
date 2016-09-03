aa927d14ac2d6a419e3c377185de77b7f2598178
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2016-09-03 03:19:49 -0300

    Lista de chat y mensajes
    Vinculación de los chats con firebase.
    En proceso de vincular los mensajes de cada chat.
    Pendiente: ver cómo recuperar el nombre del usuario emisor del mensaje.
    Continuar con los botones para el alta.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,10 +15,77 @@ angular.module('starter.controllers', [])
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
+
+
+
+
+
+
+
+    var refUsers = new Firebase("https://chatapp-4d07a.firebaseio.com/chats");
+    $scope.chats = [];
+
+    refUsers.on('child_added', function(snapshot){
+        // $timeout(function(){
+        var chat = snapshot.val();
+        var d = new Date(chat.timestamp);
+        chat.hora = d.getHours()+":"+d.getMinutes();
+        chat.id = snapshot.key();
+        $scope.chats.push(chat);
+        console.log($scope.chats);
+        // }
+    });
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
+
+
+
+
+
+    var refMensajes = new Firebase("https://chatapp-4d07a.firebaseio.com/mensajes");
+    refMensajes.on('child_added', function(snapshot){
+       
+        if(snapshot.key() == $stateParams.chatId){
+            $scope.mensajes = [];
+
+            var refMensajesDelChat = refMensajes.child($stateParams.chatId);
+            refMensajesDelChat.on('child_added', function(snapshot){
+                var mensaje = snapshot.val();
+
+                var idUsuarioEmisor = mensaje.emisor;
+                //var nombreUsuarioEmisorref = new Firebase("https://chatapp-4d07a.firebaseio.com/mensajes");;
+
+                var d = new Date(mensaje.timestamp);
+                mensaje.hora = d.getHours()+":"+d.getMinutes();
+
+//BAHHHH COMO SE RECUPERA EL NOMBREEEEEE!!!
+
+                refMensajesDelChat.root().child('usuarios/'+idUsuarioEmisor+'/nombre')
+                    .once("value", function(snapshot) {
+                        var data = snapshot.val();
+                        //alert(data);
+                        return data;
+                    });
+                
+                $scope.mensajes.push(mensaje);
+            });
+        };
+
+
+
+        // $timeout(function(){
+        // var chat = snapshot.val();
+        // var d = new Date(chat.timestamp);
+        // chat.hora = d.getHours()+":"+d.getMinutes();
+        // $scope.mensajes.push(chat);
+        // console.log($scope.mensajes);
+        // }
+    });
+
+
+
 })
 
 .controller('AccountCtrl', function($scope) {
